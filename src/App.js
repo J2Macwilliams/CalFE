e161ee00ba28c7e897bbbd9acfe42fdae100decc
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-20 20:19:56 -0800

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Splash from './components/Splash/index.js'
+import Home from './components/Home/index.js';
+import Authenticate from './components/Auth/Authenticate'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/">
+        <Splash />
+      </Route>
+      <Route path="/authenticate/google">
+        <Authenticate />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
     </div>
   );
 }
